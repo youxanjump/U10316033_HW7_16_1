e3c79e85f7be88377826b9b72101a6701205d476
youxanjump <maydayisgod@hotmail.com.tw>
2015-05-03 01:08:12 +0800

    程式執行完畢
    用迴圈匯入圖片
    原本是用迴圈直接顯示圖片
    可是到時候要重新洗牌的時候不能重洗
    所以將其存到card1~4在顯示
    setIcon的時候用card1.set............
    就可以重洗
    可是一開始我沒設card
    我set的時候我是直接
    new JLabel(poker[deck[i]]).setIcon(poker[deck[i]])
    然後用迴圈跑就不能重洗
    不明白為什麼不可以@@
    感覺只是多一個先存到card裡面而已

--- pickFourCards.java
+++ pickFourCards.java
@@ -5,71 +5,30 @@ import java.awt.event.*;
 public class pickFourCards extends JFrame{
 
 	int[] deck = new int[52];
+	ImageIcon[] poker = new ImageIcon[52];	
+	private JLabel card1;
+	private JLabel card2;
+	private JLabel card3;
+	private JLabel card4;
 	
 	private JButton jbtRefresh = new JButton("Refresh");
-	private ImageIcon poker0 = new ImageIcon("image/card/1.png");
-	private ImageIcon poker1 = new ImageIcon("image/card/2.png");
-	private ImageIcon poker2 = new ImageIcon("image/card/3.png");
-	private ImageIcon poker3 = new ImageIcon("image/card/4.png");
-	private ImageIcon poker4 = new ImageIcon("image/card/5.png");
-	private ImageIcon poker5 = new ImageIcon("image/card/6.png");
-	private ImageIcon poker6 = new ImageIcon("image/card/7.png");
-	private ImageIcon poker7 = new ImageIcon("image/card/8.png");
-	private ImageIcon poker8 = new ImageIcon("image/card/9.png");
-	private ImageIcon poker9 = new ImageIcon("image/card/10.png");
-	private ImageIcon poker10 = new ImageIcon("image/card/11.png");
-	private ImageIcon poker11 = new ImageIcon("image/card/12.png");
-	private ImageIcon poker12 = new ImageIcon("image/card/13.png");
-	private ImageIcon poker13 = new ImageIcon("image/card/14.png");
-	private ImageIcon poker14 = new ImageIcon("image/card/15.png");
-	private ImageIcon poker15 = new ImageIcon("image/card/16.png");
-	private ImageIcon poker16 = new ImageIcon("image/card/17.png");
-	private ImageIcon poker17 = new ImageIcon("image/card/18.png");
-	private ImageIcon poker18 = new ImageIcon("image/card/19.png");
-	private ImageIcon poker19 = new ImageIcon("image/card/20.png");
-	private ImageIcon poker20 = new ImageIcon("image/card/21.png");
-	private ImageIcon poker21 = new ImageIcon("image/card/22.png");
-	private ImageIcon poker22 = new ImageIcon("image/card/23.png");
-	private ImageIcon poker23 = new ImageIcon("image/card/24.png");
-	private ImageIcon poker24 = new ImageIcon("image/card/25.png");
-	private ImageIcon poker25 = new ImageIcon("image/card/26.png");
-	private ImageIcon poker26 = new ImageIcon("image/card/27.png");
-	private ImageIcon poker27 = new ImageIcon("image/card/28.png");
-	private ImageIcon poker28 = new ImageIcon("image/card/29.png");
-	private ImageIcon poker29 = new ImageIcon("image/card/30.png");
-	private ImageIcon poker30 = new ImageIcon("image/card/31.png");
-	private ImageIcon poker31 = new ImageIcon("image/card/32.png");
-	private ImageIcon poker32 = new ImageIcon("image/card/33.png");
-	private ImageIcon poker33 = new ImageIcon("image/card/34.png");
-	private ImageIcon poker34 = new ImageIcon("image/card/35.png");
-	private ImageIcon poker35 = new ImageIcon("image/card/36.png");
-	private ImageIcon poker36 = new ImageIcon("image/card/37.png");
-	private ImageIcon poker37 = new ImageIcon("image/card/38.png");
-	private ImageIcon poker38 = new ImageIcon("image/card/39.png");
-	private ImageIcon poker39 = new ImageIcon("image/card/40.png");
-	private ImageIcon poker40 = new ImageIcon("image/card/41.png");
-	private ImageIcon poker41 = new ImageIcon("image/card/42.png");
-	private ImageIcon poker42 = new ImageIcon("image/card/43.png");
-	private ImageIcon poker43 = new ImageIcon("image/card/44.png");
-	private ImageIcon poker44 = new ImageIcon("image/card/45.png");
-	private ImageIcon poker45 = new ImageIcon("image/card/46.png");
-	private ImageIcon poker46 = new ImageIcon("image/card/47.png");
-	private ImageIcon poker47 = new ImageIcon("image/card/48.png");
-	private ImageIcon poker48 = new ImageIcon("image/card/49.png");
-	private ImageIcon poker49 = new ImageIcon("image/card/50.png");
-	private ImageIcon poker50 = new ImageIcon("image/card/51.png");
-	private ImageIcon poker51 = new ImageIcon("image/card/52.png");
+	JPanel panel = new JPanel();
+	JPanel panel1 = new JPanel();
 	
 	public pickFourCards(){
+		
+		for(int i = 0 ; i < poker.length ; i ++){
+			poker[i] = new ImageIcon((getClass().getResource("image/card/" + (i+1) +".png")));
+		}
+		
+		
+		panel.add(jbtRefresh);
+		
+		panel1.setLayout(new GridLayout(1,4,1,1));
+		
 		for(int i = 0; i < deck.length; i++)
 			deck[i] = i;
 			
-		ImageIcon[] card = {poker0,poker1,poker2,poker3,poker4,poker5,poker6,poker7,poker8,poker9,
-							poker10,poker11,poker12,poker13,poker14,poker15,poker16,poker17,poker18,poker19,
-							poker20,poker21,poker22,poker23,poker24,poker25,poker26,poker27,poker28,poker29,
-							poker30,poker31,poker32,poker33,poker34,poker35,poker36,poker37,poker38,poker39,
-							poker40,poker41,poker42,poker43,poker44,poker45,poker46,poker47,poker48,poker49,
-							poker50,poker51};
 				  
 		for(int i = 0; i < deck.length; i++){
 			int index = (int)(Math.random() * deck.length);
@@ -77,22 +36,49 @@ public class pickFourCards extends JFrame{
 			deck[i] = deck[index];
 			deck[index] = temp;
 		}
+
+		card1 = new JLabel(poker[deck[0]]);
+		card2 = new JLabel(poker[deck[1]]);
+		card3 = new JLabel(poker[deck[2]]);
+		card4 = new JLabel(poker[deck[3]]);
 		
-		JPanel panel = new JPanel();
-		panel.add(jbtRefresh);
-		JPanel panel1 = new JPanel();
-		panel1.setLayout(new GridLayout(1,4,5,5));
-		for(int i = 0; i < 4; i++){
-			panel1.add(new JButton(card[deck[i]]));
-		}
+		panel1.add(card1);
+		panel1.add(card2);
+		panel1.add(card3);
+		panel1.add(card4);
 		
 		this.add(panel,BorderLayout.SOUTH);
 		this.add(panel1,BorderLayout.CENTER);
 		
+		jbtRefresh.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+			
+			panel1.repaint();
+			for(int i = 0; i < deck.length; i++)
+				deck[i] = i;
+			
+				  
+			for(int i = 0; i < deck.length; i++){
+				int index = (int)(Math.random() * deck.length);
+				int temp = deck[i];
+				deck[i] = deck[index];
+				deck[index] = temp;
+			}	
+			
+			card1.setIcon(poker[deck[0]]);
+			card2.setIcon(poker[deck[1]]);
+			card3.setIcon(poker[deck[2]]);
+			card4.setIcon(poker[deck[3]]);
+
+			}
+		});
+		
 	}
 	
+	
+	
 	public static void main(String args[]){
-		pickFourCards frame = new pickFourCards();
+		JFrame frame = new pickFourCards();
 		frame.setTitle("Pick Four Cards");
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -101,3 +87,4 @@ public class pickFourCards extends JFrame{
 	}
 
 }
+
